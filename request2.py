e0516f3ed5b43129ec1445506c116b066a9a39c7
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-22 14:56:13 +0800

    First commit

--- request2.py
+++ request2.py
@@ -1,18 +1,23 @@
 import requests
 import json
 
+# Read the Api_key
+API_KEY = open("API_Key.txt", "r").read().strip()
+
 def main():
     payload={"base":"USD","symbols":"INR"}
-    response=requests.get('https://api.exchangeratesapi.io/latest',params=payload)
-    if response.status_code !=200:
-        print("Status Code :",response.status_code)
-        raise Exception("There was error!")
     
-    print("Header:",response.headers['Content-Type'])
-    data=response.json()
-    print("JSON data",data)
-    
-    
-    
+    url = f"http://api.exchangeratesapi.io/latest?access_key={API_KEY}"
+    response = requests.get(url, verify=False,params=payload)
+    if response.status_code != 200:
+        print("Status Code:", response.status_code)
+        raise Exception("There was an error!")
+
+    print("Header:", response.headers['Content-Type'])
+    data = response.json()
+    print("JSON data:", data)
+
+
 if __name__ == "__main__":
     main()
+    
