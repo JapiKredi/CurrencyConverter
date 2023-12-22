1e789feca249101868916b55217849bc7615e714
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-22 13:39:20 +0800

    First commit

--- request.py
+++ request.py
@@ -1,25 +1,21 @@
 import requests
 import json
 
-
-API_KEY = "YOUR_API_KEY"  # Replace with your actual API key
-
 # Read the Api_key
-API_KEY = open("OpenAI_API_Key.txt", "r").read().strip()
-
+API_KEY = open("API_Key.txt", "r").read().strip()
 
 def main():
-    response=requests.get('https://api.exchangeratesapi.io/latest')
-    if response.status_code !=200:
-        print("Status Code :",response.status_code)
-        raise Exception("There was error!")
-    
-    print("Header:",response.headers['Content-Type'])
-    data=response.json()
-    print("JSON data",data)
-    
-    
-    
+    url = f"https://api.exchangeratesapi.io/latest?access_key={API_KEY}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Status Code:", response.status_code)
+        raise Exception("There was an error!")
+
+    print("Header:", response.headers['Content-Type'])
+    data = response.json()
+    print("JSON data:", data)
+
 if __name__ == "__main__":
     main()
+
     
