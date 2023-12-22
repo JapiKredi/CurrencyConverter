f2f458c33dd555fbc4b8621b5cc17d9a92012773
JapiKredi <88824661+JapiKredi@users.noreply.github.com>
2023-12-22 20:41:27 +0800

    First commit

--- request3.py
+++ request3.py
@@ -24,30 +24,3 @@ def get_currency_value(currency_symbol):
 currency_symbol = 'INR'
 currency_value = get_currency_value(currency_symbol)
 print(f"The value of {currency_symbol} is: {currency_value}")
-
-
-
-import requests
-import json
-
-# Read the Api_key
-API_KEY = open("API_Key.txt", "r").read().strip()
-
-def main():
-    url = f"http://api.exchangeratesapi.io/latest?access_key={API_KEY}"
-    response = requests.get(url, verify=False)
-    if response.status_code != 200:
-        print("Status Code:", response.status_code)
-        raise Exception("There was an error!")
-
-    print("Header:", response.headers['Content-Type'])
-    data = response.json()
-    print("JSON data:", data)
-
-    # Select the value of 'INR'
-    inr_value = data['rates']['INR']
-    print("INR value:", inr_value)
-
-
-if __name__ == "__main__":
-    main()
